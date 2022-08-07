4bb7c4249ae83ff9063cf3183465a7ed4925459b
Broadan26 <53629256+Broadan26@users.noreply.github.com>
2022-08-06 22:52:07 -0700

    Finished Day 7 Part Two

--- src/main/java/day_seven/Day7PartTwo.java
+++ src/main/java/day_seven/Day7PartTwo.java
@@ -3,6 +3,13 @@ package day_seven;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class Day7PartTwo {
 
     private final Logger logger;
@@ -16,5 +23,59 @@ public class Day7PartTwo {
     public Day7PartTwo(String filePath) {
         this.filePath = filePath;
         logger = LoggerFactory.getLogger(Day7PartTwo.class);
+    }
+
+    public long calculateFuelCost() {
+        // Read the file and get the inputs
+        var horizontalList = readFile();
+
+        // Sort the inputs
+        horizontalList.sort(Comparator.naturalOrder());
+
+        // Find the minimum number of changes between min and max
+        long minFuelCost = Long.MAX_VALUE;
+        int position = Integer.MAX_VALUE;
+        for (int target = horizontalList.get(0); target < horizontalList.get(horizontalList.size()-1); target++) {
+            long fuelCost = 0L;
+            for (Integer item: horizontalList) {
+                // Find needed moves
+                int moves = Math.abs(item - target);
+
+                // Use Euler's Series Summation to get fuel cost
+                fuelCost += ((long) moves * (moves + 1) / 2);
+            }
+            if (fuelCost < minFuelCost) {
+                minFuelCost = fuelCost;
+                position = target;
+            }
+        }
+
+        logger.info("Minimum Changes: " + minFuelCost + ", Position: " + position);
+        return minFuelCost;
+    }
+
+    /**
+     * Reads the file in the class and organizes into a list of integers
+     *
+     * @return A list of integers from the inputted file
+     */
+    private ArrayList<Integer> readFile() {
+        File file = new File(filePath);
+        var numsList = new ArrayList<Integer>();
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                int[] inputArray = Stream.of(line.split(",")).mapToInt(Integer::parseInt).toArray();
+
+                for (int val: inputArray) {
+                    numsList.add(val);
+                }
+            }
+        } catch (Exception ex) {
+            logger.info(ex.toString());
+        }
+        return numsList;
     }
 }
--- src/test/java/day_seven/Day7PartTwoTest.java
+++ src/test/java/day_seven/Day7PartTwoTest.java
@@ -3,23 +3,28 @@ package day_seven;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Day7PartTwoTest {
 
     @Test
     @DisplayName("Test Small Input")
     void testFileInputSmall() {
-
+        Day7PartTwo day7 = new Day7PartTwo("D:\\Projects\\AdventOfCode2021\\Input\\input_day_7_small");
+        assertEquals(168, day7.calculateFuelCost());
     }
 
     @Test
     @DisplayName("Test Medium Input")
     void testFileInputMedium() {
-
+        Day7PartTwo day7 = new Day7PartTwo("D:\\Projects\\AdventOfCode2021\\Input\\input_day_7_medium");
+        assertEquals(2671360, day7.calculateFuelCost());
     }
 
     @Test
     @DisplayName("Test Full Input")
     void testFileInputLarge() {
-
+        Day7PartTwo day7 = new Day7PartTwo();
+        assertEquals(91257582, day7.calculateFuelCost());
     }
 }
