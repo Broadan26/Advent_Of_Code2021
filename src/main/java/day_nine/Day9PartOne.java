cac6475504b4c1b5c5dab1cdb0aad1c9a8f43853
Broadan26 <53629256+Broadan26@users.noreply.github.com>
2023-01-05 13:16:13 -0700

    Created a working file parser for day 9 part one.

--- src/main/java/day_nine/Day9PartOne.java
+++ src/main/java/day_nine/Day9PartOne.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Day9PartOne {
 
@@ -27,6 +28,38 @@ public class Day9PartOne {
      * @return An integer value representing the total risk of all the lowest points
      */
     public int calculateRiskOfLowestPoint() {
+        // Parse the input file into a map of digits
+        var digitMap = readDigitMap();
+
         return -1;
+    }
+
+    /**
+     * Reads the file in the class and organizes it into a more workable map for searching.
+     * @return A list of the list of digits indicating lowest/highest points from 0-9
+     */
+    private ArrayList<ArrayList<Integer>> readDigitMap() {
+        File file = new File(filePath);
+        var digitMap = new ArrayList<ArrayList<Integer>>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Create new index for the digits
+                digitMap.add(new ArrayList<Integer>());
+
+                // Split each digit into its own array index
+                String[] digitArray = line.split("", 1000);
+
+                // Convert each array index into an integer and append to the map
+                for (int i = 0; i < digitArray.length-1; i++) {
+                    digitMap.get(digitMap.size()-1).add(Integer.parseInt(digitArray[i]));
+                }
+            }
+        } catch (Exception ex) {
+            logger.info(ex.toString());
+        }
+        return digitMap;
     }
 }
