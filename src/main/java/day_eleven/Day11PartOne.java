47e2d35491e50831835b5268d0b1033f2ea98d00
Broadan26 <53629256+Broadan26@users.noreply.github.com>
2023-01-06 15:19:07 -0700

    Octopi map successfully created from file parsing in Day 11.

--- src/main/java/day_eleven/Day11PartOne.java
+++ src/main/java/day_eleven/Day11PartOne.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Day11PartOne {
 
@@ -22,23 +23,44 @@ public class Day11PartOne {
         logger = LoggerFactory.getLogger(Day11PartOne.class);
     }
 
-    public int calculate() {
-        readFile();
+    /**
+     * Calculates the number of flashes that the given input of octopi produce after 100 steps have occurred.
+     * @return An integer representing the number of flashes
+     */
+    public int calculateOctopiFlashes() {
+        var octopusMap = readFile();
+
+
 
         return -1;
     }
 
-    private void readFile() {
+    /**
+     * Takes the input file and converts it into a map to use for performing calculations
+     * @return A list of a list of integers representing the energy levels of octopi
+     */
+    private ArrayList<ArrayList<Integer>> readFile() {
         File file = new File(filePath);
+        var octopusMap = new ArrayList<ArrayList<Integer>>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
 
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                logger.info(line);
+                var octopusLine = new ArrayList<Integer>();
+
+                // Convert the input line into a list of integer values
+                for (int i = 0; i < line.length(); i++) {
+                    octopusLine.add(Character.getNumericValue(line.charAt(i)));
+                }
+                // Add the line of octopi to the map if the line has octopi
+                if (!octopusLine.isEmpty()) {
+                    octopusMap.add(octopusLine);
+                }
             }
         } catch (Exception ex) {
             logger.info(ex.toString());
         }
+        return octopusMap;
     }
 }
--- src/test/java/day_eleven/Day11PartOneTest.java
+++ src/test/java/day_eleven/Day11PartOneTest.java
@@ -11,13 +11,13 @@ class Day11PartOneTest {
     @DisplayName("Test Small Input")
     void testFileInputSmall() {
         Day11PartOne day11 = new Day11PartOne("D:\\Projects\\AdventOfCode2021\\Input\\input_day_11_small");
-        assertEquals(-1, day11.calculate());
+        assertEquals(-1, day11.calculateOctopiFlashes());
     }
 
     @Test
     @DisplayName("Test Full Input")
     void testFileInputLarge() {
         Day11PartOne day11 = new Day11PartOne();
-        assertEquals(-1, day11.calculate());
+        assertEquals(-1, day11.calculateOctopiFlashes());
     }
 }
